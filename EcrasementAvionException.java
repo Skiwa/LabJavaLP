02d1ec6c7481770b9c310aaecc8ec0ae1659673f
charles33u <anne-laure.charles1@uqac.ca>
2017-12-08 16:00:35 -0500

    Exceptions à vérifier

--- EcrasementAvionException.java
+++ EcrasementAvionException.java
@@ -1,5 +1,16 @@
 import java.lang.Exception;
 
-public class EcrasementAvionException extends java.lang.Exception{
-  //surcharger la méthode getMessage()
+public class EcrasementAvionException extends Exception{
+	//surcharger la méthode getMessage()
+	//System.out.println(e.getMessage());
+	
+	public EcrasementAvionException(String message) {
+        super(message);
+    }
+	
+	public String getMessage(String message){
+		//retouner le message sous la forme ("L'avion"+nom de l'avion+" s'est écrasé !")
+		return message;
+	}
+	
 }

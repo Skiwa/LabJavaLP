0097436e62d62d8ef3f38b282c9a6419e234f8cc
charles33u <anne-laure.charles1@uqac.ca>
2017-12-08 14:30:34 -0500

    Interface Tour de Controle

--- TourControle.java
+++ TourControle.java
@@ -1,8 +1,12 @@
 //Interface
-public interface TourControle
-{
-	//Afficher l’aéroport
-	//Faire atterrir un avion
+public interface TourControle{
+	//Afficher l’aéroport ou Ciel ?->liste des avions
+	public String afficheCiel();
+	
+	//Faire atterrir un avion (supprimer du ciel)
+	public void supprAvion(int numeroSerie);
+	
 	//Faire survoler les avions (passer un tour)
+	public void tourCiel();
   
 }
